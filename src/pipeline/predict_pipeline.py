6a4b0cdd9e4518e7c2eb809e6b7a763db1f1311a
vihangajaya <vihangajayasuriya@gmail.com>
2025-01-18 16:47:47 +0100

    Model debugged and fixed all errors

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -3,57 +3,56 @@ import pandas as pd
 from src.exception import CustomException
 from src.utils import load_object
 
-
-
 class PredictPipeline:
     def __init__(self):
         pass
 
-    def predict(self,features):
-        model_path = "artifacts\model.pkl"
-        preprocessor_path = 'artifacts\preprocessor.pkl'
-        model = load_object(file_path = model_path)
-        preprocessor= load_object(file_path = preprocessor_path)
-        data_scaled = preprocessor.transform(features)
-        preds = model.predict(data_scaled)
-        return preds
+    def predict(self, features):
+        try:
+            model_path = "artifacts\model.pkl"
+            preprocessor_path = 'artifacts\proprocessor.pkl'
+            model = load_object(file_path=model_path)
+            preprocessor = load_object(file_path=preprocessor_path)
+            data_scaled = preprocessor.transform(features)
+            preds = model.predict(data_scaled)
+            return preds
+        except Exception as e:
+            raise CustomException(e, sys)
 
 class CustomData:
 
     # These come from the web page, where we input the items
-    def __init__(self,
-        gender : str,
-        race_ethnicity : str,
-        parental_level_of_education,
+    def __init__(
+        self,
+        gender: str,
+        race_ethnicity: str,
+        parental_level_of_education: str,
         lunch: str,
-        test_prepartion_course:str,
+        test_preparation_course: str,  # Fixed the spelling here
         reading_score: int,
-        writing_score: int):
+        writing_score: int,
+    ):
         self.gender = gender
         self.race_ethnicity = race_ethnicity
         self.parental_level_of_education = parental_level_of_education
         self.lunch = lunch
-        self.test_prepartion_course = test_prepartion_course
+        self.test_preparation_course = test_preparation_course
         self.reading_score = reading_score
         self.writing_score = writing_score
-
 
     def get_data_as_data_frame(self):
         try:
             custom_data_input_dict = {
-                "gender" : [self.gender],
-                "race_ethnicity" : [self.race_ethnicity],
-                "parental_level_of_education" :[self.parental_level_of_education],
-                "lunch" : [self.lunch],
-                "test_prepartion_course":[self.test_prepartion_course],
-                "read_score" : [self.reading_score],
-                "writing_score" : [self.writing_score],
-
-
+                "gender": [self.gender],
+                "race/ethnicity": [self.race_ethnicity],  # Updated to match the dataset
+                "parental level of education": [self.parental_level_of_education],  # Updated to match the dataset
+                "lunch": [self.lunch],
+                "test preparation course": [self.test_preparation_course],  # Updated to match the dataset
+                "reading score": [self.reading_score],  # Updated to match the dataset
+                "writing score": [self.writing_score],  # Updated to match the dataset
             }
 
             return pd.DataFrame(custom_data_input_dict)
         
         except Exception as e:
-            raise(CustomException(e,sys))
-        
+            raise CustomException(e, sys)
